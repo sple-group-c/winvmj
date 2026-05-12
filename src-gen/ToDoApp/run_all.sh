f4849eb6d6c954723354b2ea3b1e3e0f7c6b09ce
MHibriziF <hibrizifarghana@gmail.com>
2026-05-12 20:47:53 +0700

    feat: update todo app and project management

--- src-gen/ToDoApp/run_all.sh
+++ src-gen/ToDoApp/run_all.sh
@@ -10,9 +10,9 @@ trap cleanup SIGINT
 
 read -p "Enter the path to the frontend directory: " frontend_dir
 
-echo "SELECT 'CREATE DATABASE schedulingmanagementsystem_product_todoapp' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'schedulingmanagementsystem_product_todoapp') \gexec" | psql "postgresql://:@localhost"
+echo "SELECT 'CREATE DATABASE schedulingmanagementsystem_product_todoapp' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'schedulingmanagementsystem_product_todoapp') \gexec" | psql "postgresql://postgres:masukaja@localhost"
 for file in sql/*.sql; do
-    psql -a -f "$file" "postgresql://:@localhost/schedulingmanagementsystem_product_todoapp"
+    psql -a -f "$file" "postgresql://postgres:masukaja@localhost/schedulingmanagementsystem_product_todoapp"
 done
 
 java -cp schedulingmanagementsystem.product.todoapp --module-path schedulingmanagementsystem.product.todoapp -m schedulingmanagementsystem.product.todoapp &
